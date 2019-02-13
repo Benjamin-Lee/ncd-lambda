055c009ede33a6ecf67590c649e6c166688912b7
Kristen <keberts@college.harvard.edu>
2019-02-13 00:15:58 -0500

    new parser

--- parser.py
+++ parser.py
@@ -2,10 +2,11 @@ with open("fedpapers.txt", "r") as f:
     tmp = []
     i = 1
     for line in f:
-        tmp.append(line)
-        if "FEDERALIST No." in line:
-             new_file = open('%d.txt' %i, 'w')
-             new_file.write(''.join(tmp))
-             new_file.close()
-             i += 1
-             tmp = []
+        if "FEDERALIST" not in line:
+            tmp.append(line)
+        else:
+            new_file = open('%d.txt' %i, 'w')
+            new_file.write(''.join(tmp))
+            new_file.close()
+            i += 1
+            tmp = []
